4db8c546cd1063e96227bad4fba581f6dfd32add
Aleksandr Lagutenko <lag.aa.418@gmail.com>
2024-05-19 20:02:55 +0300

    feat: Add settings for the admin panel

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
-from .models import Category
-from .models import Quiz
-from .models import Question
-from .models import Option
-from .models import Answer
+from .models import Category, Quiz, Question, Option
+
+
+class OptionInline(admin.TabularInline):
+    model = Option
+    extra = 1
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 5
+
+
+class QuizAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [OptionInline]
+    list_filter = ["quiz"]
+
 
 admin.site.register(Category)
-admin.site.register(Quiz)
-admin.site.register(Question)
-admin.site.register(Option)
-admin.site.register(Answer)
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
