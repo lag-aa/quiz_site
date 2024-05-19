dbf348717ec4a07a147a5b230adde42a544facdc
Aleksandr Lagutenko <lag.aa.418@gmail.com>
2024-05-19 03:58:28 +0300

    feat: Add template and view for taking the quiz

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.quiz_list, name="quiz_list")
+    path("", views.quiz_list, name="quiz_list"),
+    path("<int:quiz_id>/take/", views.take_quiz, name="take_quiz"),
+    path("<int:quiz_id>/save/", views.save_quiz_answers, name="save_quiz_answers"),
+    path("<int:quiz_id>/results/", views.quiz_result, name="quiz_result"),
 ]
--- quizzes/views.py
+++ quizzes/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Quiz, Category
+from django.shortcuts import render, redirect
+from .models import Quiz, Category, Answer, Option
 
 
 def quiz_list(request):
@@ -8,3 +8,38 @@ def quiz_list(request):
     context = {"quizzes": quizzes, "categories": categories}
 
     return render(request, "quizzes/quiz_list.html", context)
+
+
+def take_quiz(request, quiz_id):
+    quiz = Quiz.objects.get(id=quiz_id)
+    questions = quiz.questions.all()
+    return render(
+        request, "quizzes/take_quiz.html", {"quiz": quiz, "questions": questions}
+    )
+
+
+def save_quiz_answers(request, quiz_id):
+    quiz = Quiz.objects.get(id=quiz_id)
+    questions = quiz.questions.all()
+    if request.method == "POST":
+        Answer.objects.filter(question__in=questions).delete()  # Clear previous answers
+        for question in questions:
+            if question.question_type == "TEXT":
+                user_answer = request.POST.get(f"question_{question.id}")
+                Answer.objects.create(question=question, text_answer=user_answer)
+            elif question.question_type == "RADIO":
+                selected_option_id = request.POST.get(f"question_{question.id}")
+                if selected_option_id:
+                    option = Option.objects.get(id=selected_option_id)
+                    Answer.objects.create(question=question, selected_option=option)
+            elif question.question_type == "CHECKBOX":
+                selected_option_ids = request.POST.getlist(f"question_{question.id}")
+                for option_id in selected_option_ids:
+                    option = Option.objects.get(id=option_id)
+                    Answer.objects.create(question=question, selected_option=option)
+        return redirect("quiz_result", quiz_id=quiz.id)
+    return redirect("take_quiz", quiz_id=quiz.id)
+
+
+def quiz_result(request, quiz_id):
+    pass
